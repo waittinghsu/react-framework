5970f1148b08ec59cf2229f30254647e870bdea3
waitting <spidergod23@gamil.com>
2024-08-15 17:27:07 +0800

    [feat] gitHub pages 刷新問題

--- src/pages/ColorPage.jsx
+++ src/pages/ColorPage.jsx
@@ -1,7 +1,39 @@
-const ColorPage = () => (
-  <div>
-    <h2>Color Page</h2>
-  </div>
-);
+import { useEffect, useInsertionEffect, useLayoutEffect, useState } from 'react';
+
+
+const ColorPage = () => {
+  const [count, setCount] = useState(0);
+
+  useInsertionEffect(() => {
+    console.log('useInsertionEffect: 副作用執行', count);
+
+    return () => {
+      console.log('useInsertionEffect: 清理函數執行', count);
+    };
+  }, [count]);
+
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect: 副作用執行', count);
+
+    return () => {
+      console.log('useLayoutEffect: 清理函數執行', count);
+    };
+  }, [count]);
+
+  useEffect(() => {
+    console.log('useEffect: 副作用執行', count);
+
+    return () => {
+      console.log('useEffect: 清理函數執行', count);
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
 
 export default ColorPage;
